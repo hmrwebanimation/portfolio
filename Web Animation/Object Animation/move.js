cd407a2d3f0d2116f40c8f61685354517d468edc
hmrwebanimation <hmrezwangraphics@gmail.com>
2020-03-04 16:16:42 +0600

    webfiles

---
+++ Web Animation/Object Animation/move.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+   
+    $('button').click(function(){
+        
+        $('.bird').css("transform","translate(240%,0%)").css("animation-play-state", "running");
+        $('.bird').css("transition","7s");
+    });
+});
---
+++ Web Animation/Snow Animation/greet.js
@@ -0,0 +1,6 @@
+function insertGreet(){
+			document.getElementById('cardName1').innerHTML = document.getElementById("first-name").value;
+			document.getElementById('cardName2').innerHTML = document.getElementById('last-name').value;
+			document.getElementById('cardName3').innerHTML = document.getElementById('greetings').value;
+			
+			}
